665eb1c6e270321aba2c41a247fabfb462d4331d
JeffLee <perfect513847@gmail.com>
2026-06-09 20:35:13 -0700

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.cpp
@@ -0,0 +1,32 @@
+class Solution {
+   public:
+    int longestConsecutive(vector<int>& nums) {
+        int result = 0;
+        unordered_set<int> s;  // we use set to store our numbers in
+        for (int i = 0; i < nums.size(); i++) {
+            s.insert(nums[i]);
+        }
+        // sort(s.begin(), s.end()); ->bug;  set cannot be sorted,
+        // while set is sort automatically, but it needs more runtime
+
+        unordered_set<int>::iterator it;
+        for (it = s.begin(); it != s.end(); it++) {
+            // **** it is a pointer, must dereference so we can use it
+            int num = *it;
+            if (!s.count(num - 1)) {
+                // if there's no number exactly 1 smaller than
+                // the current one, that means it's a potential to be a head, and
+                // might be the longest consecutive sequence
+
+                int length = 1; // we start counting the length
+                while (s.count(num + 1)) {
+                    length++;
+                    num++;
+                }
+                result = max(result, length);
+            }
+        }
+
+        return result;
+    }
+};
