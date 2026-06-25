3e1342eed9f5ed609aff818e1b9d69b50680f7c5
JeffLee <perfect513847@gmail.com>
2026-06-25 00:11:46 -0700

    Add: palindrome-permutation - submission-1

---
+++ Data Structures & Algorithms/palindrome-permutation/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    bool canPermutePalindrome(string s) {
+        int count[128] = {0};
+
+        for (int i = 0; i < s.size(); i++) {
+            count[s[i]]++;
+        }
+        int oddCount = 0;
+        for (int i = 0; i < 128; i++){
+            if(count[i] % 2){
+                oddCount +=1;
+            }
+        }
+
+        if(oddCount > 1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+};
