1ae559426919b9a06ec4eb7ed01959b069de4b7c
JeffLee <perfect513847@gmail.com>
2026-06-26 00:00:54 -0700

    Add: palindrome-permutation - submission-5

---
+++ Data Structures & Algorithms/palindrome-permutation/submission-5.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    bool canPermutePalindrome(string s) {
+        unordered_map<char, int> freq;
+        for (int i = 0; i < s.size(); i++) {
+            freq[s[i]] += 1;
+        }
+        unordered_map<char, int>::iterator it;
+
+        int oddCount = 0;
+        for (it = freq.begin(); it != freq.end(); it++) {
+            if ((it->second) % 2) {
+                oddCount += 1;
+            }
+        }
+        return oddCount <= 1;
+    }
+};
